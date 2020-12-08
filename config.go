95e9a8a900ad3ac218ce3b583424eed1eba0a9a6
simonKings <37493968+SimonWang00@users.noreply.github.com>
2020-12-08 18:23:23 +0800

    Update config.go

--- config.go
+++ config.go
@@ -2,71 +2,53 @@ package goeureka
 
 import (
 	"strings"
-	"time"
 )
 
 //File  : config.go
 //Author: Simon
-//Describe: describle your function
+//Describe: the config for eureka client
 //Date  : 2020/12/7
 
 // define eureka config
-var configStr = `
-{
-	'instanceId': '${ipAddress}:${appName}:${port}',
-	'hostName': '${ipAddress}',
-	'app': '${appName}',
-	'ipAddr': '${ipAddress}',
-	'port': {
-		'$': ${port},
-		'@enabled': 'true'
-	},
-	'securePort': {
-		'$': ${securePort},
-		'@enabled': 'false'
-	},
-	'countryId': 1,
-	'dataCenterInfo': {
-		'@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-		'name': 'MyOwn'
-	},
-	'leaseInfo': {
-		'renewalIntervalInSecs': 30,
-		'durationInSecs': 90,
-		'registrationTimestamp': 0,
-		'lastRenewalTimestamp': 0,
-		'evictionTimestamp': 0,
-		'serviceUpTimestamp': 0
-	},
-	'metadata': {
-		'management.port': '${port}'
-	},
-	'homePageUrl': 'http://${ipAddress}:${port}/',
-	'statusPageUrl': 'http://${ipAddress}:${port}/info',
-	'healthCheckUrl': 'http://${ipAddress}:${port}/health',
-	'secureHealthCheckUrl': '',
-	'vipAddress': '${appName}',
-	'secureVipAddress': '${appName}',
-	'isCoordinatingDiscoveryServer': 'false',
-	'status': 'UP',
-	'overriddenstatus': 'UNKNOWN',
-	'lastUpdatedTimestamp': '${nano}',
-	'lastDirtyTimestamp': '${nano}'
-}
-`
+var configStr = `{
+  "instance": {
+	"instanceId" : "${ipAddress}:${appName}:${port}",
+    "hostName":"${ipAddress}",
+    "app":"${appName}",
+    "ipAddr":"${ipAddress}",
+    "vipAddress":"${appName}",
+	"overriddenstatus": "UNKNOWN",
+    "status":"UP",
+    "port": {
+      "$":${port},
+      "@enabled": true
+    },
+    "securePort": {
+      "$":${securePort},
+      "@enabled": false
+    },
+    "homePageUrl" : "http://${ipAddress}:${port}/",
+    "statusPageUrl": "http://${ipAddress}:${port}/info",
+    "healthCheckUrl": "http://${ipAddress}:${port}/health",
+    "dataCenterInfo" : {
+      "@class":"com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo",
+      "name": "MyOwn"
+    },
+    "metadata": {
+      "management.port" : "${port}"
+    }
+  }
+}`
+
 
 
 // newConfig load cfg from configStr
 func newConfig(appName, port, securePort string)  string{
 	// load config
 	cfg := string(configStr)
-	nano := string(time.Now().UnixNano()/1e6)
 	cfg = strings.Replace(cfg, "${ipAddress}", getLocalIP(), -1)
 	cfg = strings.Replace(cfg, "${port}", port, -1)
 	cfg = strings.Replace(cfg, "${securePort}", securePort, -1)
-	cfg = strings.Replace(cfg, "${instanceId}", instanceId, -1)
 	cfg = strings.Replace(cfg, "${appName}", appName, -1)
-	cfg = strings.Replace(cfg, "${nano}", nano, -1)
-
 	return cfg
 }
